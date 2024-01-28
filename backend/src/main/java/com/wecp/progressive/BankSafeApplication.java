98343de8207302519c33355c588e51da7fc840f2
username <wecp-dummy@gmail.com>
2024-01-28 15:53:13 +0000

    day 4

--- backend/src/main/java/com/wecp/progressive/BankSafeApplication.java
+++ backend/src/main/java/com/wecp/progressive/BankSafeApplication.java
@@ -1,5 +1,8 @@
 package com.wecp.progressive;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BankSafeApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to BankSafe Project!");
